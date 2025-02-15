d59e783db6f3630894e35befe47c744dc98f8d34
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2025-02-15 20:45:11 +0530

    initial completion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,50 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 const ibmPlexSans = localFont({
-    src: [
-        { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
-        { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
-        { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
-        { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
-    ],
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
 });
 
 const bebasNeue = localFont({
-    src: [{ path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" }],
-    variable: "--bebas-neue",
+  src: [
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue",
 });
 
 export const metadata: Metadata = {
-    title: "BookWise",
-    description: "A simple book management app",
+  title: "BookWise",
+  description:
+    "BookWise is a book borrowing university library management solution.",
 };
 
-const RootLayout = ({
-    children,
-}: {
-    children: ReactNode;
-}) => {
-    return (
-        <html lang="en">
-            <body >{children}</body>
-        </html>
-    );
-}
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+
+  return (
+    <html lang="en">
+      <SessionProvider session={session}>
+        <body
+          className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+        >
+          {children}
+
+          <Toaster />
+        </body>
+      </SessionProvider>
+    </html>
+  );
+};
 
 export default RootLayout;
